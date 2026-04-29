6b1d5765838c4697326d177454a91e18b85baa65
chacha86 <taejincha0619@gmail.com>
2026-04-29 15:10:21 +0900

    27

---
+++ src/main/kotlin/ch27/Main.kt
@@ -0,0 +1,29 @@
+package com.back.ch27
+
+import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyles
+
+fun main() {
+
+    val num = 5
+
+    // apply => this를 받고 객체를 반환
+    // let => it을 받고 람다의 결과값 반환
+    // also => it을 받고 객체를 반환
+    // run => this를 받고 람다 값을 반환
+
+    val rst = num.run {
+        this * 2 + 10
+    }
+
+//    println(rst)
+
+//    val post = Post("Kotlin", "Kotlin is great!", "John Doe")
+//    val postDto = post.run{
+//        PostDto(
+//            title = title,
+//            content = content,
+//            author = author
+//        )
+//    }
+
+}
