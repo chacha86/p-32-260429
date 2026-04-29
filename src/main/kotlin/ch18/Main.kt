ddd7cb9e88d3f3f79f82942fbea124eb9c53cb2e
chacha86 <taejincha0619@gmail.com>
2026-04-29 12:23:15 +0900

    18

---
+++ src/main/kotlin/ch18/Main.kt
@@ -0,0 +1,14 @@
+package com.back.ch18
+
+fun main() {
+    val day = 4
+
+    val dayName = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        else -> "Invalid day"
+    }
+
+    println("Day: $dayName")
+}
